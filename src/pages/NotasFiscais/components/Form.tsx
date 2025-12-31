b42c87e438476ade4f6955647de56dfa0b52579d
adm01-debug <adm01@promobrindes.com.br>
2025-12-30 21:11:16 -0300

    feat: add Form.tsx

---
+++ src/pages/NotasFiscais/components/Form.tsx
@@ -0,0 +1,13 @@
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
+
+export function NFForm() {
+  return (
+    <Dialog>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Emitir NFe</DialogTitle>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
