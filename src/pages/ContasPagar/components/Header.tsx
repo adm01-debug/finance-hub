c30321c4970e588603fb442ab53ee320b547d9f5
adm01-debug <adm01@promobrindes.com.br>
2025-12-30 21:04:58 -0300

    feat: add Header.tsx

---
+++ src/pages/ContasPagar/components/Header.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button';
+import { Plus } from 'lucide-react';
+
+export function ContasPagarHeader() {
+  return (
+    <div className="flex justify-between items-center">
+      <h1 className="text-3xl font-bold dark:text-white">Contas a Pagar</h1>
+      <Button>
+        <Plus className="w-4 h-4 mr-2" />
+        Nova Conta
+      </Button>
+    </div>
+  );
+}
