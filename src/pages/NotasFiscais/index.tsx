0e34a9a2cfd340a2bcdd91e5e1534a8a61072f23
adm01-debug <adm01@promobrindes.com.br>
2025-12-31 11:51:13 -0300

    feat: add/update index.tsx

--- src/pages/NotasFiscais/index.tsx
+++ src/pages/NotasFiscais/index.tsx
@@ -1,15 +1,17 @@
 import { NFProvider } from './context';
 import { NFHeader } from './components/Header';
+import { NFFilters } from './components/Filters';
 import { NFList } from './components/List';
-import { NFForm } from './components/Form';
+import { NFEmissaoModal } from './components/EmissaoModal';
 
 export default function NotasFiscais() {
   return (
     <NFProvider>
       <div className="space-y-6 p-6">
         <NFHeader />
+        <NFFilters />
         <NFList />
-        <NFForm />
+        <NFEmissaoModal />
       </div>
     </NFProvider>
   );
