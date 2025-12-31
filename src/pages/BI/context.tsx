b796a96dfab15e9cf2ab29747e657b6f8d1e8668
adm01-debug <adm01@promobrindes.com.br>
2025-12-31 11:51:11 -0300

    feat: add/update context.tsx

--- src/pages/BI/context.tsx
+++ src/pages/BI/context.tsx
@@ -1,9 +1,9 @@
-import { createContext, useContext, ReactNode } from 'react';
+import { createContext, useContext } from 'react';
 import { useBIData } from './hooks/useBIData';
 
 const BIContext = createContext<any>(null);
 
-export function BIProvider({ children }: { children: ReactNode }) {
+export function BIProvider({ children }: { children: React.ReactNode }) {
   const value = useBIData();
   return <BIContext.Provider value={value}>{children}</BIContext.Provider>;
 }
