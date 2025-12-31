ba3a2bbdac944a9607d071a5b59e9ab499f027a7
adm01-debug <adm01@promobrindes.com.br>
2025-12-30 21:07:48 -0300

    chore: add Form.tsx

---
+++ src/pages/ContasPagar/components/Form.tsx
@@ -0,0 +1,14 @@
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
+
+export function ContaPagarForm() {
+  return (
+    <Dialog>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Nova Conta a Pagar</DialogTitle>
+        </DialogHeader>
+        {/* Form content */}
+      </DialogContent>
+    </Dialog>
+  );
+}
