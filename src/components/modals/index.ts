e34241d71fac0afe7ceb5918bbeaeec4ec159cee
adm01-debug <adm01@promobrindes.com.br>
2026-01-21 10:09:45 -0300

    feat: add src/components/modals/index.ts

---
+++ src/components/modals/index.ts
@@ -0,0 +1,4 @@
+export { ContaPagarModal } from './conta-pagar-modal';
+export { ContaReceberModal } from './conta-receber-modal';
+export { ClienteModal } from './cliente-modal';
+export { FornecedorModal } from './fornecedor-modal';
