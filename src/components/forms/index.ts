a058014528b9456bd1c778d0a296574e60ca19c3
adm01-debug <adm01@promobrindes.com.br>
2026-01-21 10:09:38 -0300

    feat: add src/components/forms/index.ts

---
+++ src/components/forms/index.ts
@@ -0,0 +1,23 @@
+// Form Fields & Components
+export {
+  FormField,
+  TextInput,
+  TextArea,
+  Select,
+  Checkbox,
+  RadioGroup,
+} from './form-field';
+
+// Masked Input
+export { MaskedInput } from './masked-input';
+
+// Currency Input
+export { CurrencyInput } from './currency-input';
+
+// Date Input
+export { DateInput } from './date-input';
+
+// Entity Forms
+export { ClienteForm } from './cliente-form';
+export { FornecedorForm } from './fornecedor-form';
+export { ContaPagarForm } from './conta-pagar-form';
